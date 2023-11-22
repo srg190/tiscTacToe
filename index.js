fd833df5510f791bfa1f3484858d1413212ebfed
sudhi <sudhi@shriffle.com>
2023-11-23 01:25:19 +0530

    --fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 ////////////
 import { setTheme } from "./Theme/theme.js";
-import { Alert } from "./components/alert/index.js";
+// import { Alert } from "./components/alert/index.js";
 import Events from "./events.js";
 import { playGame } from "./page/game/index.js";
 
@@ -8,6 +8,6 @@ import { playGame } from "./page/game/index.js";
   setTheme();
   playGame(false);
 })();
-let alert = new Alert();
-alert.CreatingAlert();
+// let alert = new Alert();
+// alert.CreatingAlert();
 Events();
